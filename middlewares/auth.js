d08ebb7e0f0123f1bee9befe460763d50d2da0b2
Abin <abin>
2024-08-05 19:42:12 +0530

    bugs fixed

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,13 +4,12 @@ dotenv.config();
 
 const auth = (req, res, next) => {
     const token = req.headers.authorization
-    console.log(token);
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY_USER);
-        req.userId = decoded.userId;
+        req.body.userId = decoded.userId;
         next();
     } catch (err) {
         res.status(401).json({ message: 'Invalid token.' });
