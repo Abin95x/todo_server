b21790d13d8fdff5960ef83ec4871ccdd1eab4cc
Abin <abin>
2024-08-06 14:08:24 +0530

    todo add and delete functionality added

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,14 +3,12 @@ import { Todo } from '../models/todoModel.js'
 export const addTodo = async (req, res) => {
     try {
         const { task, description, userId, projectId } = req.body
-        console.log('Validation failed:', { task, description, userId, projectId });
 
          if (!task || !userId || !projectId || !description) {
             return res.status(400).json({ message: 'Task, Description, Project Id User Id are required.' });
         }
 
         const existingTodo = await Todo.findOne({ task, description, userId, projectId });
-        console.log(existingTodo);
         
         if (existingTodo) {
             return res.status(409).json({ message: 'A todo with already exists for this user and project.' });
@@ -35,7 +33,7 @@ export const addTodo = async (req, res) => {
 export const getTodos = async (req, res) => {
     try {
         const { projectId } = req.query
-        console.log(projectId);
+
         if ( !projectId ) {
             return res.status(400).json({ message: 'Project Id required.' });
         }
@@ -47,4 +45,42 @@ export const getTodos = async (req, res) => {
         return res.status(500).json({ message: 'Server error.', error: error.message });
     }
 }
+
+
+export const markTodo = async (req, res) => {
+    try {
+        const { todoId } = req.query
+        const todo = await Todo.findById(todoId);
+        
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found.' });
+        }
+        todo.status = (todo.status === 'pending') ? 'done' : 'pending';
+        await todo.save();
+
+        return res.status(200).json({ message: 'Todo status updated successfully.', todo });
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error.', error: error.message });
+    }
+}
+
+export const deleteTodo = async (req, res) => {
+    try {
+        const { todoId } = req.query;
+        const todo = await Todo.findById(todoId);
+
+        if (!todo) {
+            return res.status(404).json({ message: 'Todo not found.' });
+        }
+
+        await Todo.findByIdAndDelete(todoId);
+        return res.status(200).json({ message: 'Todo deleted successfully.' });
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error.', error: error.message });
+    }
+}
+
+
 
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -3,17 +3,20 @@ import sanitizeInput from '../middlewares/sanitization.js'
 import auth from '../middlewares/auth.js'
 import {
     addTodo,
-    getTodos
+    getTodos,
+    markTodo,
+    deleteTodo
 
-    
 } from '../controllers/todoController.js'
 
 const todoRouter = express.Router()
 
 todoRouter.post('/addtodo', auth, sanitizeInput, addTodo)
 todoRouter.get('/gettodos', auth, getTodos)
-todoRouter.patch('/updatetodo', auth, sanitizeInput, )
-todoRouter.put('/deletetodo', auth, )
+todoRouter.patch('/marktodo', auth, markTodo)
+
+// todoRouter.patch('/updatetodo', auth, sanitizeInput,)
+todoRouter.put('/deletetodo', auth, deleteTodo)
 
 
 
